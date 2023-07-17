6e34881e729cb5bbdf0c41021789a5bd7b4b93a5
Fabián Machuca <20019496+Gfabianmm@users.noreply.github.com>
2023-07-17 07:03:16 -0500

    avance en consumo de api, login y registro

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {UserRegister} from './userRegister.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './Services/auth.service';
 import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import {RouteService} from './Services/route.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { HttpClient ,HttpHeaders} from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private modalService: NgbModal, public authService: AuthService,private http: HttpClient) {}
+  constructor(private modalService: NgbModal, public authService: AuthService,private http: HttpClient, private routeService: RouteService) {}
 
   title = 'Test Bango Guayaquil - Gerardo Machuca Morán';
   user?: User;
@@ -39,31 +40,26 @@ export class AppComponent {
   }
 
   OnRegister(modal: any) {
-
-    this.user = {
-      id:1, // Asigna un ID adecuado según tu lógica de generación de ID
-      // displayName:this.userRegister.names,
-      name: this.userRegister.names,
-      mail: this.userRegister.mail,
-      interests:[]
-      // Agrega más propiedades según sea necesario
-    };
-    this.authService.login(this.user);
-    modal.close('Registro exitoso');
-    // console.log(userRegister.)
+    this.authService.RegisterUser(this.userRegister);
+     modal.close();
   }
-
   OnLogIn(modal: any) {
-
-    this.user = {
-      id:1,
-      name: 'Gerardo F. Mahcuca Morán',
-      mail: this.userRegister.mail,
-      interests:[]
-    };
-    this.authService.login(this.user);
-    modal.close();
+    this.authService.login(this.userRegister)
+      .then((user: User | null) => {
+        if (user) {
+          this.user = user;
+          console.log(user);
+          modal.close();
+        } else {
+          alert('El usuario no existe');
+        }
+      })
+      .catch((error: any) => {
+        alert('Ocurrió un error al verificar el usuario: ' + error);
+      });
   }
+
+
   onLogout() {
     // Cerrar sesión
     this.authService.logout();
@@ -71,7 +67,8 @@ export class AppComponent {
   }
 
   fetchMovieData() {
-    const url = 'https://localhost:7259/api/Movie?IsSortAscending=true&Page=-1';
+
+    const url = this.routeService.GetMovieUri()+'?IsSortAscending=true&Page=-1';
     const headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
     this.http.get(url,{ headers }).subscribe((response: any) => {
 
